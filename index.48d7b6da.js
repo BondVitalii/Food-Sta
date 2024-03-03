9f0cfbe9d940ac47531712018d48758623d51316
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2024-03-03 12:36:33 +0000

    Deploying to gh-pages from @ BondVitalii/Food-Sta@5a7c82e1ac3e6c551ba1a4d7482534c3b4719f6e 🚀

--- index.48d7b6da.js
+++
@@ -1,2 +0,0 @@
-window.onscroll=function(){return e=document.querySelector("[data-header]"),n=e.offsetHeight,void(window.pageYOffset>n?e.classList.add("no-transparency"):e.classList.remove("no-transparency"));var e,n};
-//# sourceMappingURL=index.48d7b6da.js.map
--- index.b8a08a29.js
+++
@@ -1,2 +0,0 @@
-window.onscroll=()=>function(){const e=document.querySelector("[data-header]"),n=e.offsetHeight;window.pageYOffset>n?e.classList.add("no-transparency"):e.classList.remove("no-transparency")}();
-//# sourceMappingURL=index.b8a08a29.js.map
